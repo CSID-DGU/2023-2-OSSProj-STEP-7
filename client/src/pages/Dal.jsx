7e099a1c816f3275d1bd8297c55ebc12d368692f
ifuiw <alswlk05@dgu.ac.kr>
2023-12-10 16:06:06 +0900

    Refactor: 달력 디자인 개선

--- client/src/pages/Dal.jsx
+++ client/src/pages/Dal.jsx
@@ -54,9 +54,15 @@ class Dal extends Component {
   
 
   render() {
+
+    const wrapperStyle = {
+      marginLeft: '100px',  // 좌측 여백
+      marginRight: '100px'  // 우측 여백
+    };
+  
     return (
       <div className="mypage-body">
-        <div className="body-wrapper box">
+        <div className="body-wrapper box" style={wrapperStyle}>
           <div className="body-info-container">
             <div className="calendar-wrapper">
               <FullCalendar
@@ -76,7 +82,9 @@ class Dal extends Component {
                     right: 'dayGridMonth,timeGridWeek,timeGridDay'
                   }}
 
-
+                  contentHeight={900} // 달력 내용의 높이를 500px로 고정
+                  aspectRatio={1.5} // 가로 세로 비율을 조절, 1.5는 예시이므로 필요에 따라 수정
+                  // height={800}
                   navLinks={ true} // can click day/week names to navigate views
                   selectables={ true} 
                   selectMirrors={ true} 
